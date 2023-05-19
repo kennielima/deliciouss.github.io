3abdc156e2859fbc989ae8ee8ed91f63b01629fb
kennielima <kennylima970@gmail.com>
2023-05-19 20:43:30 +0100

    deliciouss
    new

---
+++ src/UI/Card.jsx
@@ -0,0 +1,10 @@
+// import React from 'react';
+// import styled from "./Card.module.css";
+
+// function Card(props) {
+//   return (
+//     <div className={styled.card}>{props.children}</div>
+//   )
+// }
+
+// export default Card;
---
+++ src/UI/Wrapper.jsx
@@ -0,0 +1,10 @@
+// import React from 'react';
+// import styled from "./Wrapper.module.css";
+
+// function Wrapper(props) {
+//   return (
+//     <div className={styled.wrapper}>{props.children}</div>
+//   )
+// }
+
+// export default Wrapper;
